8055d1d6db1dafa77947be07584a0e5bfe5e4bb6
Tiaonmmn <tiaonmmn@163.com>
2020-04-07 09:15:39 +0800

    Initial commit

---
+++ service/settings/run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+service nginx stop
+mv /etc/nginx/sites-enabled/default /tmp/
+mv /tmp/nginx-flask.conf /etc/nginx/sites-enabled/flask
+
+service nginx restart
+
+uwsgi /home/src/uwsgi.ini
+#/bin/bash /home/cleaner.sh &
+
+/bin/cat /dev/null
+while true
+do
+	sleep 5s
+done
